ec27e42a7b37728c1be2f9099913d8eea91e12a5
Денис <d-e-n-i-s-ne@yandex.ru>
2023-07-28 13:14:08 +0300

    заполнил pom и добавил ErrorHandler

---
+++ finteh/src/main/java/org/example/exceptions/BadRequestException.java
@@ -0,0 +1,9 @@
+package org.example.exceptions;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+
+        super(message);
+    }
+}
